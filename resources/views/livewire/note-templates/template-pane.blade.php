b303c8440ee3efbdb06247855c48db552e85918f
DuarteJose <duartejose020202@gmail.com>
2023-09-14 15:59:53 -0300

    fix

--- resources/views/livewire/note-templates/template-pane.blade.php
+++ resources/views/livewire/note-templates/template-pane.blade.php
@@ -120,7 +120,7 @@
                 </svg>
                 Delete
             </button>
-            <a type="button" href="{{route("wave.templates.edit", $template->id)}}"
+            {{-- <a type="button" href="{{route("wave.templates.edit", $template->id)}}"
                 class="{{ $mode == "edit" ? 'disabled' : ""}} flex gap-1 items-center text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                     stroke="currentColor" class="w-4 h-4">
@@ -128,7 +128,7 @@
                         d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
                 </svg>
                 Edit
-            </a>
+            </a> --}}
             <button type="button"
                 class="flex gap-1 items-center text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
