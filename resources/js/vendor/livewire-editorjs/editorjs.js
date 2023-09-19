1fcdbaa8c670c61ae2b47f4fa99dafb9098a0c06
SuperDev0123 <superdev1230@outlook.com>
2023-09-19 15:30:55 -0300

    fix: simplify editor js

--- resources/js/vendor/livewire-editorjs/editorjs.js
+++ resources/js/vendor/livewire-editorjs/editorjs.js
@@ -1,5 +1,4 @@
 import EditorJS from '@editorjs/editorjs';
-import ImageTool from '@editorjs/image';
 import Header from '@editorjs/header';
 import Underline from '@editorjs/underline';
 import Code from '@editorjs/code';
@@ -10,6 +9,7 @@ import NestedList from '@editorjs/nested-list';
 import Checklist from '@editorjs/checklist';
 import ColorPlugin from 'editorjs-text-color-plugin';
 import { StyleInlineTool } from 'editorjs-style';
+// import ImageTool from '@editorjs/image';
 
 window.editorInstance = function (dataProperty, editorId, readOnly, placeholder, logLevel) {
     return {
@@ -29,51 +29,51 @@ window.editorInstance = function (dataProperty, editorId, readOnly, placeholder,
                 logLevel,
 
                 tools: {
-                    image: {
-                        class: ImageTool,
+                    // image: {
+                    //     class: ImageTool,
 
-                        config: {
-                            uploader: {
-                                uploadByFile: (file) => {
-                                    return new Promise((resolve) => {
-                                        this.$wire.upload(
-                                            'uploads',
-                                            file,
-                                            (uploadedFilename) => {
-                                                const eventName = `fileupload:${uploadedFilename.substr(0, 20)}`;
+                    //     config: {
+                    //         uploader: {
+                    //             uploadByFile: (file) => {
+                    //                 return new Promise((resolve) => {
+                    //                     this.$wire.upload(
+                    //                         'uploads',
+                    //                         file,
+                    //                         (uploadedFilename) => {
+                    //                             const eventName = `fileupload:${uploadedFilename.substr(0, 20)}`;
 
-                                                const storeListener = (event) => {
-                                                    resolve({
-                                                        success: 1,
-                                                        file: {
-                                                            url: event.detail.url
-                                                        }
-                                                    });
+                    //                             const storeListener = (event) => {
+                    //                                 resolve({
+                    //                                     success: 1,
+                    //                                     file: {
+                    //                                         url: event.detail.url
+                    //                                     }
+                    //                                 });
 
-                                                    window.removeEventListener(eventName, storeListener);
-                                                };
+                    //                                 window.removeEventListener(eventName, storeListener);
+                    //                             };
 
-                                                window.addEventListener(eventName, storeListener);
+                    //                             window.addEventListener(eventName, storeListener);
 
-                                                this.$wire.call('completedImageUpload', uploadedFilename, eventName);
-                                            }
-                                        );
-                                    });
-                                },
+                    //                             this.$wire.call('completedImageUpload', uploadedFilename, eventName);
+                    //                         }
+                    //                     );
+                    //                 });
+                    //             },
 
-                                uploadByUrl: (url) => {
-                                    return this.$wire.loadImageFromUrl(url).then(result => {
-                                        return {
-                                            success: 1,
-                                            file: {
-                                                url: result
-                                            }
-                                        }
-                                    });
-                                }
-                            }
-                        }
-                    },
+                    //             uploadByUrl: (url) => {
+                    //                 return this.$wire.loadImageFromUrl(url).then(result => {
+                    //                     return {
+                    //                         success: 1,
+                    //                         file: {
+                    //                             url: result
+                    //                         }
+                    //                     }
+                    //                 });
+                    //             }
+                    //         }
+                    //     }
+                    // },
                     list: {
                         class: NestedList,
                         inlineToolbar: true,
@@ -88,9 +88,9 @@ window.editorInstance = function (dataProperty, editorId, readOnly, placeholder,
                         },
                     },
                     underline: Underline,
-                    code: Code,
-                    'inline-code': InlineCode,
-                    quote: Quote,
+                    // code: Code,
+                    // 'inline-code': InlineCode,
+                    // quote: Quote,
                     // warning: Warning,
                     paragraph: {
                         class: Paragraph,
@@ -109,7 +109,7 @@ window.editorInstance = function (dataProperty, editorId, readOnly, placeholder,
                             customPicker: true // add a button to allow selecting any colour  
                         }
                     },
-                    style: StyleInlineTool,
+                    // style: StyleInlineTool,
                 },
 
                 data: this.data,
