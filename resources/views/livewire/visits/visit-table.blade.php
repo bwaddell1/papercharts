e6ffaf7520a7b81600b60159d422bd2c41a8be4d
SuperDev0123 <superdev1230@outlook.com>
2023-09-29 20:17:17 -0300

    fix

--- resources/views/livewire/visits/visit-table.blade.php
+++ resources/views/livewire/visits/visit-table.blade.php
@@ -237,7 +237,7 @@
                         <!-- Modal header -->
                         <div class="flex items-start justify-between p-4 border-b rounded-t">
                             <h3 class="text-xl font-semibold text-gray-900">
-                                Complete Visit Electronically
+                                Complete Visit Electronically {{$generate_note_id}}
                             </h3>
                             <button type="button" wire:click="closeQuickNoteModal"
                                 class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center">
