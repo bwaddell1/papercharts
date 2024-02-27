615045f94913cdd144761c6c79c3ccd700288e82
SuperDev0123 <superdev1230@outlook.com>
2024-02-27 07:15:08 -0500

    fix: subscription migrations

--- wave/database/migrations/2018_09_22_234251_create_subscriptions_table.php
+++ wave/database/migrations/2018_09_22_234251_create_subscriptions_table.php
@@ -2,39 +2,36 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
-class CreateSubscriptionsTable extends Migration {
+class CreateSubscriptionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('subscriptions', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('team_id');
+            $table->string('name');
+            $table->string('stripe_id')->unique();
+            $table->string('stripe_status');
+            $table->string('stripe_price')->nullable();
+            $table->integer('quantity')->nullable();
+            $table->timestamp('trial_ends_at')->nullable();
+            $table->timestamp('ends_at')->nullable();
+            $table->timestamps();
 
-	/**
-	 * Run the migrations.
-	 *
-	 * @return void
-	 */
-	public function up()
-	{
-		Schema::create('subscriptions', function(Blueprint $table)
-		{
-			$table->increments('id');
-			$table->integer('user_id')->unsigned();
-			$table->string('name');
-			$table->string('stripe_id');
-			$table->string('stripe_plan');
-			$table->integer('quantity');
-			$table->dateTime('trial_ends_at')->nullable();
-			$table->dateTime('ends_at')->nullable();
-			$table->timestamps();
-		});
-	}
+            $table->index(['team_id', 'stripe_status']);
+        });
+    }
 
-
-	/**
-	 * Reverse the migrations.
-	 *
-	 * @return void
-	 */
-	public function down()
-	{
-		Schema::dropIfExists('subscriptions');
-	}
-
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('subscriptions');
+    }
 }
